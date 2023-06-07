9c46f88b59397f8ced0c84d0ecdd6719b1e8ff3c
吳珮綾 <songgaqp44@gmail.com>
2023-06-07 19:26:23 +0800

    3

---
+++ Web410928092/Web410928092/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410928092.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: final
+        public ActionResult Exam()
+        {
+            return View();
+        }
+    }
+}
